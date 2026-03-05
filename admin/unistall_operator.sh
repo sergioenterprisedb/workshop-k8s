333086fe39c8ad5a48e5971101024b604a379c72
Sergio Romera <sergio.romera@enterprisedb.com>
2026-03-05 16:20:16 +0100

    Version 1.0

---
+++ admin/unistall_operator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+version1=`${kubectl_cnp} version | awk '{ print $2 }' | awk -F":" '{ print $2}'`
+version2=${version1%??}
+
+# Uninstall operator
+kubectl delete -f \
+  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-${version2}/releases/cnpg-${version1}.yaml
+
