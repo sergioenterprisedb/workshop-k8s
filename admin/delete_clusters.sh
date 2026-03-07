140c89fb8a38a89dbf174c46dcbb734308070553
Sergio Romera <sergio.romera@enterprisedb.com>
2026-03-07 16:26:08 +0100

    Version 1.0

--- admin/delete_clusters.sh
+++ admin/delete_clusters.sh
@@ -6,5 +6,6 @@
 for (( i=1; i<=$TOTAL_USERS; i++ ))
 do
   username="user$i"
+  sudo su - ${username} -c "kubectl delete backup  cluster-${username}-backup-test"
   sudo su - ${username} -c "kubectl delete cluster cluster-${username}"
 done
