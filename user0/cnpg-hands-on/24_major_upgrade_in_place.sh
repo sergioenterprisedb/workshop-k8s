0bc870d7808f0e90bfb7e1893643a3b0afe571fc
Sergio Romera <sergio.romera@enterprisedb.com>
2026-03-09 18:59:46 +0100

    Version 1.0

--- user0/cnpg-hands-on/24_major_upgrade_in_place.sh
+++ user0/cnpg-hands-on/24_major_upgrade_in_place.sh
@@ -5,10 +5,10 @@ source ./config.sh
 # Create yaml file from template
 envsubst < templates/cluster-example-major-upgrade-inplace-template.yaml > $TMP/${cluster_name}-major-upgrade-inplace.yaml
 
-# Diff old vs new config 
+# Diff old vs new config
 clear
 print_command "diff -a --suppress-common-lines -y $TMP/${cluster-name}-minor-upgrade.yaml $TMP/${cluster_name}-major-upgrade-inplace.yaml\n"
-diff -a --suppress-common-lines -y $TMP/cluster-example-minor-upgrade.yaml $TMP/${cluster_name}-major-upgrade-inplace.yaml
+diff -a --suppress-common-lines -y $TMP/${cluster_name}-minor-upgrade.yaml $TMP/${cluster_name}-major-upgrade-inplace.yaml
 
 sleep 5
 
